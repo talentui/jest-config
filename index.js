04b17b4f3021a26b9a9019884f0ae8c12d47844d
LiGuoming <it1011@qq.com>
2017-07-21 15:13:17 +0800

    删除内置globals对象，添加参数支持，然后删除用例查找规则，使用默认即可

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const path = require("path");
 module.exports = function(options = {}) {
 
-    let appRoot = options.root || path.resolve(__dirname, "../../../");
+    let {root: appRoot = path.resolve(__dirname, "../../../"), globals } = options;
 
     return {
-        //查找测试用例
-        testMatch: ["**/tests/**/*.js?(x)"],
         //自定义如何处理js文件
         transform: {
             "^.+\\.jsx?$": path.resolve(__dirname, "./utils/processor.js")
         },
 
         moduleDirectories: [path.resolve(appRoot, "./src"), "node_modules"],
-        globals: {
-            BSGlobal: {},
-            __webpack_public_path__: "",
-            iTalentSDK: {
-                register: function() {}
-            }
-        },
+        globals: globals || {},
         moduleNameMapper: {
             //模块查找规则， 第一条与talent-ui的默认规则相同，其他的是非js模块的处理规则
             "&(.*)$": "<rootDir>/src$1",
