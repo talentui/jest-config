8af59e6cc036030dce37de6bba329c109925f51c
LiGuoming <it1011@qq.com>
2017-09-22 15:33:32 +0800

    忘了添加module extensions了

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const path = require("path");
 module.exports = function(options = {}) {
+    let { root: appRoot = process.cwd(), globals } = options;
 
-    let {root: appRoot = process.cwd(), globals } = options;
-
-    console.log(`current work directory is ${appRoot}`)
+    console.log(`current work directory is ${appRoot}`);
 
     return {
         //自定义如何处理js文件
@@ -19,10 +18,16 @@ module.exports = function(options = {}) {
         moduleNameMapper: {
             //模块查找规则， 第一条与talent-ui的默认规则相同，其他的是非js模块的处理规则
             "&(.*)$": "<rootDir>/src$1",
-            "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-                path.resolve(__dirname,'./utils/file-mock.js'),
-            "\\.(css|less|scss)$": path.resolve(__dirname,'./utils/style-mock.js')
+            "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": path.resolve(
+                __dirname,
+                "./utils/file-mock.js"
+            ),
+            "\\.(css|less|scss)$": path.resolve(
+                __dirname,
+                "./utils/style-mock.js"
+            )
         },
-        testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$"
+        testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+        moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"]
     };
 };
