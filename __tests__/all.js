be9a92c0b9b45b8e30433d6558eb01f82927e031
Raymond <it1011@qq.com>
2017-10-27 18:09:26 +0800

    test: do not use snapshot test

--- __tests__/all.js
+++ __tests__/all.js
@@ -1,4 +1,11 @@
 test('生成snapshot', () => {
     const util = require('../index.js');
-    expect(util()).toMatchSnapshot();
+    let conf = util();
+    expect(conf).toHaveProperty('transform');
+    expect(conf).toHaveProperty('moduleDirectories');
+    expect(conf).toHaveProperty('globals');
+    expect(conf).toHaveProperty('collectCoverageFrom');
+    expect(conf).toHaveProperty('moduleNameMapper');
+    expect(conf).toHaveProperty('testRegex');
+    expect(conf).toHaveProperty('moduleFileExtensions');
 })
